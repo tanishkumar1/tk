6555c6450636630ac678eedbc82483c41a2d77a5
tanishkumar1 <60348368+tanishkumar1@users.noreply.github.com>
2020-01-28 15:08:53 +0530

    Update data.php

--- data.php
+++ data.php
@@ -1,17 +1,8 @@
 <?php
-echo "Hello";
 $servername = "35.202.118.228";
-$username = "root";
+$username = "tk";
 $password = "Tanish@123";
 $dbname="reg";
-// Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
 
   $fname = $_POST["fname"];
   $lname=$_POST["lname"];
@@ -21,6 +12,15 @@ echo "Connected successfully";
   $phn=$_POST["phone"]);
   $country=$_POST["country"];
   $dob=$_POST["dob"];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 
 $sql = "INSERT INTO reg_1 (fname, lname, dob,email,pass,cpass,pn,country)
 VALUES ('$fname','$lname','$dob','$email','$pass','$cpass','$phn','$country')";
